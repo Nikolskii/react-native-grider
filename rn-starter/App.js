5cfebbb3c32a7eba5f87e39c484c34fdb33e625e
Denis Nikolsky <den-apple@ya.ru>
2023-03-18 09:21:47 +0200

    Showing a Custom Component

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -1,17 +1,19 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import HomeScreen from "./src/screens/HomeScreen";
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import ComponentsScreen from './src/screens/ComponentsScreen';
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    Components: ComponentsScreen,
   },
   {
-    initialRouteName: "Home",
+    initialRouteName: 'Components',
     defaultNavigationOptions: {
-      title: "App",
+      title: 'App',
     },
-  }
+  },
 );
 
 export default createAppContainer(navigator);
