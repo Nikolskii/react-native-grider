499e9dbf77302057f126a52f75316ce2a1d7af50
Denis Nikolsky <den-apple@ya.ru>
2023-03-20 20:01:22 +0200

    State Management in React Components

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -3,22 +3,60 @@ import { StyleSheet, View } from 'react-native';
 
 import ColorCounter from '../components/ColorCounter';
 
+const COLOR_INCREMENT = 15;
+
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
 
-  console.log(red);
+  const setColor = (color, change) => {
+    switch (color) {
+      case 'red':
+        red + change > 255 || red + change < 0 ? null : setRed(red + change);
+        return;
+
+      case 'green':
+        green + change > 255 || green + change < 0
+          ? null
+          : setGreen(green + change);
+        return;
+
+      case 'blue':
+        blue + change > 255 || blue + change < 0
+          ? null
+          : setBlue(blue + change);
+        return;
+
+      default:
+        return;
+    }
+  };
 
   return (
     <View>
       <ColorCounter
-        onIncrease={() => setRed(red + 1)}
-        onDecrease={() => setRed(red - 1)}
+        onIncrease={() => setColor('red', COLOR_INCREMENT)}
+        onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
         color="Red"
       />
-      <ColorCounter color="Green" />
-      <ColorCounter color="Blue" />
+      <ColorCounter
+        onIncrease={() => setColor('green', COLOR_INCREMENT)}
+        onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)}
+        color="Green"
+      />
+      <ColorCounter
+        onIncrease={() => setColor('blue', COLOR_INCREMENT)}
+        onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}
+        color="Blue"
+      />
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red},${green},${blue})`,
+        }}
+      />
     </View>
   );
 };
