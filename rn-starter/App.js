3f7838870c995a489404f5a0e3c34a3a9fc77e60
Denis Nikolsky <den-apple@ya.ru>
2023-03-22 22:06:07 +0200

    How to Handle Screen Layout

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -9,6 +9,7 @@ import CounterScreen from './src/screens/CounterScreen';
 import ColorScreen from './src/screens/ColorScreen';
 import SquareScreen from './src/screens/SquareScreen';
 import TextScreen from './src/screens/TextScreen';
+import BoxScreen from './src/screens/BoxScreen';
 
 const navigator = createStackNavigator(
   {
@@ -20,6 +21,7 @@ const navigator = createStackNavigator(
     Color: ColorScreen,
     Square: SquareScreen,
     Text: TextScreen,
+    Box: BoxScreen,
   },
   {
     initialRouteName: 'Home',
---
+++ rn-starter/src/screens/BoxScreen.js
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const BoxScreen = () => {
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.textOneStyle}>Child #1</Text>
+      <Text style={styles.textTwoStyle}>Child #2</Text>
+      <Text style={styles.textThreeStyle}>Child #3</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: 'black',
+    height: 200,
+    alignItems: 'center',
+  },
+  textOneStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+  },
+  textTwoStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+    alignSelf: 'flex-start',
+  },
+  textThreeStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+  },
+});
+
+export default BoxScreen;
