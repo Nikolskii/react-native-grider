41fe3deb3d40c454e3f8f16691f6d11d75a0ea36
Denis <den-apple@ya.ru>
2023-03-24 22:20:01 +0200

    Test

--- food/App.js
+++ food/App.js
@@ -4,8 +4,6 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   console.log();
 
-  console.log();
-
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
