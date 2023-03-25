3be74a7fa4f624c3864ac835ace361b48fe32342
Denis Nikolsky <den-apple@ya.ru>
2023-03-25 15:40:08 +0200

    Starting the SearchBar

---
+++ food/src/components/SearchBar.js
@@ -0,0 +1,20 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchBar = () => {
+  return (
+    <View style={styles.background}>
+      <Text>SearchBar</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: '#F0EEEE',
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+  },
+});
+
+export default SearchBar;
