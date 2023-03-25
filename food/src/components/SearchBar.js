cdf41aabfa0e576b445b591449b844c5aef390e0
Denis Nikolsky <den-apple@ya.ru>
2023-03-25 20:36:26 +0200

    Detecting Editing Completion

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -1,17 +1,26 @@
 import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
     <View style={styles.backgroundStyle}>
-      <Feather name="search" size={30} />
-      <TextInput style={styles.inputStyle} placeholder="Search" />
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   backgroundStyle: {
+    marginTop: 15,
     backgroundColor: '#F0EEEE',
     height: 50,
     borderRadius: 5,
@@ -19,9 +28,13 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
   inputStyle: {
-    borderColor: 'black',
-    borderWidth: 1,
     flex: 1,
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15,
   },
 });
 
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState('');
+
   return (
     <View>
-      <SearchBar />
+      <SearchBar
+        term={term}
+        onTermChange={newTerm => setTerm(newTerm)}
+        onTermSubmit={() => console.log('term was submitted')}
+      />
+      <Text>{term}</Text>
     </View>
   );
 };
