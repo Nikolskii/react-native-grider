e1bcc4fcf00abe2226ff86defaa92b725cc52fd3
Denis <den-apple@ya.ru>
2023-05-20 17:00:04 +0200

    Using Outside API's and Making Hooks Reusable

--- food/src/api/yelp.js
+++ food/src/api/yelp.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: '',
+  baseURL: 'https://api.yelp.com/v3/businesses',
+  headers: {
+    Authorization:
+      'Bearer qtHWDELd2rBnwDHS6rSFDOTUG9DwI7TpP64lWa-Z8CPHVbWH8oZUZ5OTCKThhhSf4Ogr4Pq0GAHAy2GmrE47ghCKF1y0U74LPzF0VqviimvEvI_XHKzNW8L08awcZHYx',
+  },
 });
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,18 +1,42 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
-const SearchScreen = () => {
+const SearchScreen = searchTerm => {
   const [term, setTerm] = useState('');
+  const [businesses, setBusinesses] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const searchApi = async () => {
+    try {
+      const responce = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: 'san jose',
+        },
+      });
+
+      setBusinesses(responce.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
+  };
+
+  useEffect(() => {
+    searchApi('pasta');
+  }, []);
 
   return (
     <View>
       <SearchBar
         term={term}
-        onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => console.log('term was submitted')}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
       />
-      <Text>{term}</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {businesses.length} businesses</Text>
     </View>
   );
 };
