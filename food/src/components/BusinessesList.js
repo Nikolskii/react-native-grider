b72919cc9b6c57f9e787bce112aca7cb5822331b
Denis <den-apple@ya.ru>
2023-05-21 09:38:57 +0200

    Navigation with Parameters (108-110)

--- food/src/components/BusinessesList.js
+++ food/src/components/BusinessesList.js
@@ -1,15 +1,16 @@
 import { View, StyleSheet, Text, FlatList } from 'react-native';
+import BusinessDetail from './BusinessDetail';
 
 const BusinessesList = ({ title, businesses }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
       <FlatList
         horizontal
         data={businesses}
         keyExtractor={business => business.id}
         renderItem={({ item }) => {
-          return <Text>{item.name}</Text>;
+          return <BusinessDetail business={item} />;
         }}
       />
     </View>
@@ -17,9 +18,14 @@ const BusinessesList = ({ title, businesses }) => {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    marginBottom: 10,
+  },
   title: {
     fontSize: 18,
     fontWeight: 'bold',
+    marginLeft: 15,
+    marginBottom: 5,
   },
 });
 
