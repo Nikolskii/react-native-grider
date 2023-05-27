5a27278b3478a3556370f558f67166347155c2cd
Denis <den-apple@ya.ru>
2023-05-27 19:09:53 +0200

    Advanced State Management with Context (132-136)

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
+      return state;
+  }
+};
+
 export const BlogProvider = ({ children }) => {
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
+
+  const addBlogPost = () => {
+    dispatch({ type: 'add_blogpost' });
+  };
+
   return (
-    <BlogContext.Provider value="Hi there!">{children}</BlogContext.Provider>
+    <BlogContext.Provider
+      value={{
+        data: blogPosts,
+        addBlogPost,
+      }}
+    >
+      {children}
+    </BlogContext.Provider>
   );
 };
 
