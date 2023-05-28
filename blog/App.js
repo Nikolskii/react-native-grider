94b1fc95fc227ac6382edf762fd7ccf004d6b26e
Denis <den-apple@ya.ru>
2023-05-28 09:23:32 +0200

    Advanced State Management with Context (137-138)

--- blog/App.js
+++ blog/App.js
@@ -1,8 +1,7 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-
 import IndexScreen from './src/screens/IndexScreen';
-import { BlogProvider } from './src/context/BlogContext';
+import { Provider as BlogProvider } from './src/context/BlogContext';
 
 const navigator = createStackNavigator(
   {
